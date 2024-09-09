f0153b8f2eb1bdcb09961a6ca451c91a8a818cd9
Godfrey <godfreydavouofficial@gmail.com>
2024-09-09 23:46:46 +0100

    added files via upload
    my portfolio

---
+++ style.js
@@ -0,0 +1,40 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+
+    // Burger Animation
+    burger.classList.toggle('toggle');
+});
+
+// Smooth Scrolling
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+
+        // Close the nav menu on mobile after clicking
+        if (nav.classList.contains('nav-active')) {
+            nav.classList.remove('nav-active');
+            burger.classList.remove('toggle');
+        }
+    });
+});
+
